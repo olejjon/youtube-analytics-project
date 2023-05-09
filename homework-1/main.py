d7542ad4130ebd4853fc134622a9320793398846
olejjon123 <olejjon_123@mail.ru>
2023-05-09 16:47:52 +0300

    task 2 completed

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,6 +2,7 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    # vdud = Channel('UCy6p8krDY33mDRMt19YrlXg') # LITVIN
     vdud.print_info()
 
     """
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,42 @@
+import json
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key = os.getenv('YT_API_KEY')
+    api_key1 = os.environ['api_key']
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.channel_info = {}
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        self.info_youtube()
+        print(self.channel_info)
+
+    def info_youtube(self) -> None:
+        youtube = build('youtube', 'v3', developerKey=Channel.api_key1)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+        # playlist_id = 'PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb'
+        # playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
+        #                                                part='contentDetails',
+        #                                                maxResults=50,
+        #                                                ).execute()
+        # video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+        # video_response = youtube.videos().list(part='contentDetails,statistics',
+        #                                        id=','.join(video_ids)
+        #                                        ).execute()
+        # for video in video_response['items']:
+        #     # YouTube video duration is in ISO 8601 format
+        #     iso_8601_duration = video['contentDetails']['duration']
+        #     duration = isodate.parse_duration(iso_8601_duration)
+        #     print(duration)
+
+        self.channel_info = json.dumps(channel, indent=2, ensure_ascii=False)
