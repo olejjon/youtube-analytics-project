f455d8be893a6f231c71ff4e0d8d49209020c23f
olejjon123 <olejjon_123@mail.ru>
2023-05-19 13:00:14 +0300

    update homework 5

--- src/playlist.py
+++ src/playlist.py
@@ -14,8 +14,8 @@ class PlayList:
     def __init__(self, id_playlist: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.id_playlist = id_playlist
-        self.playlist = PlayList.get_playlist_info(id_playlist)
-        self.title = "Редакция. АнтиТревел"  # self.playlist['snippet']['title']
+        self.playlist = self.get_service().playlists().list(id=self.id_playlist, part='snippet').execute()
+        self.title = self.playlist['items'][0]['snippet']['title']
         self.url = f"https://www.youtube.com/playlist?list={id_playlist}"
 
     def __str__(self):
@@ -25,21 +25,18 @@ class PlayList:
     def get_service(cls):
         return build('youtube', 'v3', developerKey=PlayList.api_key)
 
-    @classmethod
-    def get_playlist_info(cls, playlist_ids):
-        return PlayList.get_service().playlistItems().list(playlistId=playlist_ids,
-                                            part='snippet,contentDetails', maxResults=50,
-                                            ).execute()
+    def get_playlist_info(self, playlist_ids):
+        return self.get_service().playlist().list(playlistId=playlist_ids,
+                                                  part='snippet').execute()
 
-    @classmethod
-    def get_video_info(cls, video_ids):
-        return PlayList.get_service().videos().list(part='contentDetails,statistics', id=','.join(video_ids)).execute()
+    def get_video_info(self, video_ids):
+        return self.get_service().videos().list(part='contentDetails,statistics', id=','.join(video_ids)).execute()
 
     @property
     def total_duration(self):
-        list_videos = PlayList.get_videos_playlist(self)
-        video_response = PlayList.get_service().videos().list(part='contentDetails,statistics', id=','.join(list_videos)
-                                               ).execute()
+        list_videos = self.get_videos_playlist()
+        video_response = self.get_service().videos().list(part='contentDetails,statistics', id=','.join(list_videos)
+                                                          ).execute()
 
         summ_duration = timedelta(seconds=0)
         for video in video_response['items']:
@@ -50,17 +47,17 @@ class PlayList:
         return summ_duration
 
     def get_videos_playlist(self):
-        playlist_videos = PlayList.get_service().playlistItems().list(playlistId=self.id_playlist,
-                                                                      part='contentDetails',
-                                                                      maxResults=50, ).execute()
+        playlist_videos = self.get_service().playlistItems().list(playlistId=self.id_playlist,
+                                                                  part='contentDetails',
+                                                                  maxResults=50, ).execute()
 
         video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
         return video_ids
 
     def show_best_video(self):
-        list_videos = PlayList.get_videos_playlist(self)
-        video_response = PlayList.get_service().videos().list(part='contentDetails,statistics', id=','.join(list_videos)
-                                                              ).execute()
+        list_videos = self.get_videos_playlist()
+        video_response = self.get_service().videos().list(part='contentDetails,statistics', id=','.join(list_videos)
+                                                          ).execute()
 
         max_like = 0
         for video in video_response['items']:
